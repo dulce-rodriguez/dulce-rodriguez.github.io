1b6b75308adb36b5cc6e8f4c6bbb9cba0e9e8f06
dulce-rodriguez <rod21010@byui.edu>
2021-06-26 16:02:20 -0600

    fixed js

---
+++ lesson10/js/test.js
@@ -0,0 +1,14 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=4d690f02017296e33407357d1be1a373&units=imperial';
+
+fetch(apiURL)
+.then ((response) => response.json())
+.then((jsObject) => {
+    console.log(jsObject);
+    const temperature = document.getElementById('current-temp');
+    temperature.textContent = jsObject.main.temp.toFixed(0);
+    const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.getElementById('imagesrc').textContent = imagesrc;
+    document.getElementById('icon').setAttribute('src', imagesrc);
+    document.getElementById('icon').setAttribute('alt', desc);
+});
--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -4,11 +4,53 @@ fetch(apiURL)
 .then ((response) => response.json())
 .then((jsObject) => {
     console.log(jsObject);
+    const currently = document.getElementById('current');
+    currently.textContent = jsObject.weather[0].main;
     const temperature = document.getElementById('current-temp');
-    temperature.textContent = jsObject.main.temp;
-    const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
-    const desc = jsObject.weather[0].description;
-    document.getElementById('imagesrc').textContent = imagesrc;
-    document.getElementById('icon').setAttribute('src', imagesrc);
-    document.getElementById('icon').setAttribute('alt', desc);
+    temperature.textContent = jsObject.main.temp.toFixed(0);
+    const humidity = document.getElementById('humidity');
+    humidity.textContent = jsObject.main.humidity;
+    const WindSpeed = document.getElementById('speed');
+    WindSpeed.textContent = jsObject.wind.speed;
+
+    const WindChill = document.getElementById('wind');
+    if (temperature <= 50 && WindSpeed >= 3) {
+        WindChill.textContent = (35.74 + 0.6215 * temperature - 35.75 * (Math.pow(WindSpeed, 0.16)) + 0.4275 * temperature * (Math.pow(WindSpeed, 0.16)) + "&deg;F");
+    }
+    else {
+        WindChill.textContent = 'n/a';
+    }
+});
+
+const apiURLforecast = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=4d690f02017296e33407357d1be1a373&units=imperial';
+fetch(apiURLforecast)
+.then ((response) => response.json())
+.then((jsObject) => {
+    console.log(jsObject);
+    let day = 0;
+    let img = 0;
+    const dayofweek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const fivedayForecast = jsObject.list.filter( forecast => forecast.dt_txt.includes('18:00:00'));
+    console.log(fivedayForecast)
+
+    fivedayForecast.forEach( x => {
+        let d = new Date(x.dt_txt);
+        console.log(d);
+        document.getElementById(`dayofweek${day+1}`).textContent = dayofweek[d.getDay()];
+        document.getElementById(`forecast${day+1}`).textContent = x.main.temp;
+        day++;  
+        const imagesrc = `https://openweathermap.org/img/w/${jsObject.list[img].weather[0].icon}.png`;
+        const desc = jsObject.list[img].weather[0].description;
+        document.getElementById(`icon${img+1}`).setAttribute('src', imagesrc);
+        document.getElementById(`icon${img+1}`).setAttribute('alt', desc);
+        img++;
+    }); 
+
+    /*fivedayForecast.forEach( x => {
+        const imagesrc = `https://openweathermap.org/img/w/${jsObject.list[img].weather[0].icon}.png`;
+        const desc = jsObject.list[img].weather[0].description;
+        document.getElementById(`icon${img+1}`).setAttribute('src', imagesrc);
+        document.getElementById(`icon${img+1}`).setAttribute('alt', desc);
+        img++
+    })*/
 });
