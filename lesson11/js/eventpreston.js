4f55bb005d2aaddfeb286d0e7a78ae8bc1ebf7f3
dulce-rodriguez <rod21010@byui.edu>
2021-07-03 10:45:19 -0600

    updated erros

--- lesson11/js/eventpreston.js
+++ lesson11/js/eventpreston.js
@@ -11,37 +11,24 @@ fetch(requestURL)
         
         if (i == 6) {
             let card = document.createElement('section');
-            let h3 = document.createElement('h3');
-            let h4 = document.createElement('h4');
             let p1 = document.createElement('p');
             let p2 = document.createElement('p');
             let p3 = document.createElement('p');
-            let img = document.createElement('img');
 
     
-            card.className = "cards" + i;
-            h3.textContent = towns[i].name;
-            h4.textContent = towns[i].motto;
-            p1.textContent = "Year Founded: " + towns[i].yearFounded;
-            p2.textContent = "Population: " + towns[i].currentPopulation;
-            p3.textContent = "Annual Rainfall: " + towns[i].averageRainfall;
-            img.setAttribute('src', "images/" + towns[i].photo)
-            img.setAttribute('alt', towns[i].name + i);
+            card.className = "article";
+
+            p1.textContent = towns[i].events[0];
+            p2.textContent = towns[i].events[1];
+            p3.textContent = towns[i].events[2];
     
-            card.appendChild(h3);
-            card.appendChild(h4);
             card.appendChild(p1);
             card.appendChild(p2);
             card.appendChild(p3);
-            card.appendChild(img);
     
-            document.querySelector('div.cards').appendChild(card);
+            document.querySelector('div.town-events').appendChild(card);
 
         }
     }
-
-    document.getElementsByClassName("cards0").style.order = "2";
-    document.getElementsByClassName("cards2").style.order = "3";
-    document.getElementsByClassName("cards6").style.order = "1";
 
   });
